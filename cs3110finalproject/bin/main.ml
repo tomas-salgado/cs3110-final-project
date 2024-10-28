e66ebb06af19dd93b7ee346e9e3d343f2f5f3dc5
tch83 <tch83@cornell.edu>
2024-10-28 15:33:03 -0400

    Implementing ability for user to implement character choice in terminal initially

--- cs3110finalproject/bin/main.ml
+++ cs3110finalproject/bin/main.ml
@@ -1,4 +1,13 @@
-let () =
+let display_character_message choice =
+  match choice with
+  | 1 -> print_endline "You have chosen Kate the Knight!"
+  | 2 -> print_endline "You have chosen Walter the Wizard!"
+  | 3 -> print_endline "You have chosen Max the Monk!"
+  | 4 -> print_endline "You have chosen Abigail the Archer!"
+  | 5 -> print_endline "You have chosen Alan the Alchemist!"
+  | _ -> print_endline "Invalid choice, please enter a number between 1 and 5."
+
+let choose_character () =
   print_endline "";
   print_endline "⚔️🐪 WELCOME TO CAMELOT ADVENTURES 🐪⚔️";
   print_endline "";
@@ -19,4 +28,12 @@ let () =
     \    - (3) Max the Monk (Strength: Healing)\n\
     \    - (4) Abigail the Archer (Strength: Speed)\n\
     \    - (5) Alan the Alchemist (Strength: Potion Crafting)\n\
-    \    "
+    \    ";
+  print_string "Enter your choice (1-5): ";
+
+  match read_int_opt () with
+  | Some choice -> display_character_message choice
+  | None ->
+      print_endline "Invalid input, please enter a number between 1 and 5."
+
+let () = choose_character ()
