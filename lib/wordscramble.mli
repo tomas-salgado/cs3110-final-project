b049e6e5253a7e638992e011e30ae158cd267b5c
tch83 <tch83@cornell.edu>
2024-12-10 16:55:13 -0500

    Updating wordscramble.mli documentation

--- lib/wordscramble.mli
+++ lib/wordscramble.mli
@@ -10,7 +10,14 @@ val shuffle : 'a list -> 'a list
     random order. *)
 
 val scramble_word : string -> string
+(** [scramble_word word] is the string [word] with the letters rearranged in a
+    random order. *)
+
 val guess_loop : int -> string -> (unit -> string) -> bool
+(** [guess_loop attempts_left original_word f] is a loop that continuosly prints
+    results to screen as the user attempts to guess the scrambled word. The user
+    has [attempts_left] attempts to guess the scrambled word. The word they must
+    guess is [original_word]. *)
 
 val play_word_scramble : unit -> bool
 (** [play_word_scramble ()] initiates the word scramble game for the user at the
